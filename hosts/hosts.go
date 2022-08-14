8dc490e2a02971dfc457350b0aa6b3bc99be34fe
conkay <conkay@foxmail.com>
2022-08-14 11:18:35 +0800

    fix mix group

--- hosts/hosts.go
+++ hosts/hosts.go
@@ -21,6 +21,23 @@ type MyHosts struct {
 	Hosts     map[string]map[string]*Host `json:"hosts"`
 }
 
+type M map[string]string
+
+type HostUnique struct {
+	Hide      bool
+	IP        string
+	Hostname  string
+	GroupName M
+}
+
+func (m *M) toSlice() []string {
+	s := make([]string, 0, len(*m))
+	for _, v := range *m {
+		s = append(s, v)
+	}
+	return s
+}
+
 func NewMyHosts() *MyHosts {
 	return &MyHosts{
 		Path:      getHostPath(),
@@ -56,9 +73,6 @@ func (f *MyHosts) Split() {
 		if len(res) == 0 {
 			continue
 		}
-		if _, ok := f.Hosts[res[5]]; !ok {
-			f.Hosts[res[5]] = map[string]*Host{}
-		}
 		hide := strings.Contains(res[1], "#")
 		for _, hostname := range strings.Split(res[3], " ") {
 			hostname = strings.TrimSpace(hostname)
@@ -68,31 +82,67 @@ func (f *MyHosts) Split() {
 			if !strings.Contains(res[2], ":") && !strings.Contains(res[2], ".") {
 				continue
 			}
-			f.Hosts[res[5]][hostname] = &Host{
-				Hide:      hide,
-				IP:        res[2],
-				Hostname:  hostname,
-				GroupName: res[5],
+			var groupNames = map[string]string{}
+			for _, groupName := range strings.Split(res[5], "#") {
+				groupName = strings.TrimSpace(groupName)
+				if groupName == "" {
+					continue
+				}
+				groupNames[groupName] = groupName
 			}
-			log.Println(Host{
-				Hide:      hide,
-				IP:        res[2],
-				Hostname:  hostname,
-				GroupName: res[5],
-			})
+			if len(groupNames) == 0 {
+				groupNames = map[string]string{"uncategorized": "uncategorized"}
+			}
+			for _, groupName := range groupNames {
+				if _, ok := f.Hosts[groupName]; !ok {
+					f.Hosts[groupName] = map[string]*Host{}
+				}
+				f.Hosts[groupName][hostname] = &Host{
+					Hide:      hide,
+					IP:        res[2],
+					Hostname:  hostname,
+					GroupName: groupName,
+				}
+				log.Println(Host{
+					Hide:      hide,
+					IP:        res[2],
+					Hostname:  hostname,
+					GroupName: groupName,
+				})
+			}
 		}
 	}
 }
 
 func (f *MyHosts) Pretty() {
 	f.HostsText = ""
+	var allHostsUnique = map[string]HostUnique{}
 	for groupName, hosts := range f.Hosts {
 		for hostname, host := range hosts {
+			var key string
 			if host.Hide {
-				f.HostsText += fmt.Sprintf("# %s %s # %s\n", host.IP, hostname, groupName)
+				key = fmt.Sprintf("%s_#", hostname)
 			} else {
-				f.HostsText += fmt.Sprintf("%s %s # %s\n", host.IP, hostname, groupName)
+				key = fmt.Sprintf("%s_", hostname)
 			}
+			if _, ok := allHostsUnique[key]; !ok {
+				allHostsUnique[key] = HostUnique{
+					Hide:      host.Hide,
+					IP:        host.IP,
+					Hostname:  hostname,
+					GroupName: M{groupName: groupName},
+				}
+			} else {
+				allHostsUnique[key].GroupName[groupName] = groupName
+			}
+		}
+	}
+	for _, row := range allHostsUnique {
+		groupName := strings.Join(row.GroupName.toSlice(), " # ")
+		if row.Hide {
+			f.HostsText += fmt.Sprintf("# %s %s # %s\n", row.IP, row.Hostname, groupName)
+		} else {
+			f.HostsText += fmt.Sprintf("%s %s # %s\n", row.IP, row.Hostname, groupName)
 		}
 	}
 }
