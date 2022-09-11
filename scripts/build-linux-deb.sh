367502fe352c5e27d9b6aad8f21a710cc849fa57
conkay <conkay@foxmail.com>
2022-09-12 03:23:15 +0800

    release linux

--- scripts/build-linux-deb.sh
+++ scripts/build-linux-deb.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
-
-APP=hosts-switch
-APPNAME="Hosts Switch"
-APPDIR=${APP}_1.0.0
+release=$(printf %s "$1" | tr -d "v")
+app=hosts-switch
+app_name="Hosts Switch"
+app_dir=${app}_linux_amd64_v${release}
 
 cd ../
 
-mkdir -p build/linux/$APPDIR/usr/bin
-mkdir -p build/linux/$APPDIR/usr/share/applications
-mkdir -p build/linux/$APPDIR/usr/share/icons/hicolor/1024x1024/apps
-mkdir -p build/linux/$APPDIR/usr/share/icons/hicolor/256x256/apps
-mkdir -p build/linux/$APPDIR/DEBIAN
+mkdir -p build/linux/${app_dir}/usr/bin
+mkdir -p build/linux/${app_dir}/usr/share/applications
+mkdir -p build/linux/${app_dir}/usr/share/icons/hicolor/1024x1024/apps
+mkdir -p build/linux/${app_dir}/usr/share/icons/hicolor/256x256/apps
+mkdir -p build/linux/${app_dir}/DEBIAN
 
 wails build --clean --platform linux/amd64
-cp build/bin/hosts-switch build/linux/$APPDIR/usr/bin/${APP}
+cp build/bin/hosts-switch build/linux/${app_dir}/usr/bin/${app}
 
-cp build/appicon.png build/linux/$APPDIR/usr/share/icons/hicolor/1024x1024/apps/${APP}.png
-cp build/appicon.png build/linux/$APPDIR/usr/share/icons/hicolor/256x256/apps/${APP}.png
+cp build/appicon.png build/linux/${app_dir}/usr/share/icons/hicolor/1024x1024/apps/${app}.png
+cp build/appicon.png build/linux/${app_dir}/usr/share/icons/hicolor/256x256/apps/${app}.png
 
-cat > build/linux/$APPDIR/usr/share/applications/${APP}.desktop << EOF
+cat > build/linux/${app_dir}/usr/share/applications/${app}.desktop << EOF
 [Desktop Entry]
-Version=1.0.0
+Version=${release}
 Type=Application
-Name=$APPNAME
+Name=${app_name}
 Exec=$APP
 Icon=$APP
 Terminal=false
 StartupWMClass=Lorca
 EOF
 
-cat > build/linux/$APPDIR/DEBIAN/control << EOF
-Package: ${APP}
-Version: 1.0.0
+cat > build/linux/${app_dir}/DEBIAN/control << EOF
+Package: ${app}
+Version: ${release}
 Section: base
 Priority: optional
 Architecture: amd64
@@ -39,5 +39,5 @@ Maintainer: conkay <conkay@foxmail.com>
 Description: Hosts Switch
 EOF
 
-dpkg-deb --build build/linux/$APPDIR
-rm -rf build/linux/$APPDIR
+dpkg-deb --build build/linux/${app_dir}
+rm -rf build/linux/${app_dir}
