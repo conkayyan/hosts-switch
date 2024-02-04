80de799cb8e5186863b2bc0667cfaa7111ce67e3
conkay <conkay@foxmail.com>
2024-02-05 00:58:40 +0800

    wails build

--- scripts/build-windows.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-echo -e "Start running the script..."
-cd ../
-
-echo -e "Start building the app for windows platform..."
-wails build --clean --platform windows/amd64 -nsis -webview2 embed
-
-echo -e "End running the script!"
